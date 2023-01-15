5804fd3889061d22e84161cad084c95e8f3dfb9d
Koi_Dev <koideveloping@gmail.com>
2023-01-14 22:49:39 -0600

    map_illustrate() and top_x_sources()
    Updated the map illustration to include the 'very_large' and 'catastrophic' parameters. Created a parameter that enables users to see scales of madgnitudes (for example, only 'small' landslides)
    
    Created top_x_sources(), which allows the user to see the top x amount of sources where the data is pulled from.
    
    Added comments explaining code.

--- scripts/Landslide.py
+++ scripts/Landslide.py
@@ -7,24 +7,31 @@ import matplotlib.image as mpimg
 # Create a categorical based on landslide trigger (what percentage causes the most landslides)
 # Plot areas on a tableau map to show where the most landslides happen + size indicator
 
-
 class Landslide:
-  
+    
+    # Initializes class and puts data into a dataframe, as well as storing the map image for later use.
     def __init__(self, url, image):
         self.url = url
         self.image = plt.imread(image)
         self.data = pd.read_csv(self.url)
         self.formatting()
         
+    # Format parameters for illustrations. Used as a preset for axis and title information.
     def formatting(self):
         self.titleFormat = {'size': 30, 'weight': 'bold', 'y': 1.03}
         self.labelFormat = {'size': 20, 'weight': 'bold'}
         self.labelFormatOut = {'size': 20, 'weight': 'bold', 'labelpad': 20}
     
-    def map_illustrate(self):
+    # Shows a map illustration of where the landslide reports occurred. 
+    # Parameter [mag] - The magnitude of the landslide. Acceptable input below: 
+    # ['all', 'small', 'medium', 'large', 'very_large', 'catastrophic']
+    def map_illustrate(self, mag):
+        self.m = 20
         self.map_info = self.data[['landslide_size', 'longitude', 'latitude']]
-        self.slideSize = lambda x: 25 if x == 'small' else (65 if x == 'medium' else 110)
-        self.slideColor = lambda x: 'yellow' if x == 'small' else ('orange' if x == 'medium' else 'red')
+        if(mag != 'all'):
+            self.map_info = self.map_info[self.map_info.landslide_size == mag]
+        self.slideSize = lambda x: 1*self.m if x=='small' else (self.m*2 if x=='medium' else (4*self.m if x == 'large' else (6*self.m if x=='very_large' else (8*self.m if x=='catastrophic' else 1*self.m))))
+        self.slideColor = lambda x: '#ffff00' if x=='small' else ('#ffa500' if x=='medium' else ((1,0,0) if x=='large' else('#800000' if x=='very_large' else('#4c00b0' if x=='catastrophic' else '#ffa500'))))
         self.fig, self.ax = plt.subplots()
         self.ax.imshow(self.image, extent = [-180, 180, -60, 90])
         self.ax.scatter(self.map_info.longitude, self.map_info.latitude, s=self.map_info.landslide_size.apply(self.slideSize), alpha=0.5, c=self.map_info.landslide_size.apply(self.slideColor))
@@ -35,6 +42,19 @@ class Landslide:
         plt.xticks(fontsize = 12)
         plt.yticks(fontsize = 12)
         plt.show()
-    
+        
+    # Shows the top x sources that the reports comes from. Organizes based on the amount of reports made per country. 
+    def top_x_sources(self, x):
+        self.countries = self.data[['country_name']].reset_index()
+        self.total_val = self.countries.country_name.count()
+        self.slides = self.countries.country_name.value_counts().sort_values(ascending=False)[:x].sort_values(ascending=True).reset_index()
+        self.slides.columns = ['country_name', 'reports']
+        plt.figure(figsize = (12, 7))
+        plt.barh(self.slides.country_name, self.slides.reports, color=(0.8, 0, 0))
+        plt.title('Top {} Reporting Countries'.format(x), **self.titleFormat)
+        plt.xlabel('Amount of Reports', **self.labelFormatOut)
+        plt.ylabel('Country', **self.labelFormatOut)
+        plt.show()
+                                                                                                                                   
 lab = Landslide('Global_Landslide_Catalog_Export.csv', 'world_image_adjusted.PNG')
-lab.map_illustrate()
+lab.map_illustrate('all')
